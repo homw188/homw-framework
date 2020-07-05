9153c0ad3c54ff62c308534129b229e93c7c670a
James <james_wyi@163.com>
2020-07-05 21:28:10 +0800

    feat: add some gui components

---
+++ homw-gui/src/main/java/com/homw/gui/d2/swing/UIInitializer.java
@@ -0,0 +1,57 @@
+package com.homw.gui.d2.swing;
+
+import com.alee.laf.WebLookAndFeel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Enumeration;
+
+/**
+ * @description UI初始化器
+ * @author Hom
+ * @version 1.0
+ * @since 2020-07-05
+ */
+public class UIInitializer {
+
+    /**
+     * UI样式初始化，汉化处理，EDT(event dispatching thread)运行
+     */
+    public static void init() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    WebLookAndFeel.install();
+                    // UIManager.setLookAndFeel(WebLookAndFeel.class.getCanonicalName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                Enumeration<?> enumeration = UIManager.getDefaults().keys();
+                String keyStr = null;
+                while (enumeration.hasMoreElements()) {
+                    keyStr = enumeration.nextElement().toString();
+                    if (keyStr.toLowerCase().contains("font")) {
+                        UIManager.put(keyStr, SWT.FontConst.PLAIN_14);
+                    }
+
+                    if (keyStr.equals("TextComponent.copy")) {
+                        UIManager.put(keyStr, "复制");
+                    } else if (keyStr.equals("TextComponent.paste")) {
+                        UIManager.put(keyStr, "粘贴");
+                    } else if (keyStr.equals("TextComponent.cut")) {
+                        UIManager.put(keyStr, "剪切");
+                    }
+                }
+                UIManager.put("ProgressBar.selectionBackground", Color.black);
+                UIManager.put("ProgressBar.selectionForeground", Color.black);
+                UIManager.put("OptionPane.yesButtonText", "是");
+                UIManager.put("OptionPane.noButtonText", "否");
+                UIManager.put("OptionPane.okButtonText", "确认");
+                UIManager.put("OptionPane.cancelButtonText", "取消");
+                UIManager.put("TableUI", "javax.swing.plaf.basic.BasicTableUI");
+            }
+        });
+    }
+}
---
+++ homw-gui/src/main/java/com/homw/gui/d2/swing/util/FontUtil.java
@@ -0,0 +1,72 @@
+package com.homw.gui.d2.swing.util;
+
+import cn.hutool.core.io.IoUtil;
+import com.homw.gui.d2.swing.SWT;
+import sun.swing.SwingUtilities2;
+
+import java.awt.*;
+import java.io.InputStream;
+
+/**
+ * @description 字体工具类
+ * @author Hom
+ * @version 1.0
+ * @since 2020-07-05
+ */
+public class FontUtil {
+
+    /**
+     * 获取屏幕自适应大小字体
+     * @param isBlod
+     * @return
+     */
+    public static Font getPreferedFont(boolean isBlod) {
+        if (SWT.Size.getScreen().width > 1600) {
+            return isBlod ? SWT.FontConst.BLOD_20 : SWT.FontConst.PLAIN_20;
+        } else if (SWT.Size.getScreen().width > 1400) {
+            return isBlod ? SWT.FontConst.BLOD_18 : SWT.FontConst.PLAIN_18;
+        } else if (SWT.Size.getScreen().width > 1200) {
+            return isBlod ? SWT.FontConst.BLOD_16 : SWT.FontConst.PLAIN_16;
+        } else {
+            return isBlod ? SWT.FontConst.BLOD_14 : SWT.FontConst.PLAIN_14;
+        }
+    }
+
+    /**
+     * 创建字体
+     * @param fontFile
+     * @return
+     */
+    public static Font createFont(String fontFile) {
+        Font font = null;
+        InputStream input = null;
+        try {
+            input = FontUtil.class.getResourceAsStream(fontFile);
+            font = Font.createFont(Font.TRUETYPE_FONT, input);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            IoUtil.close(input);
+        }
+        return font;
+    }
+
+    /**
+     * 获取字符串像素长度
+     * @param font
+     * @param text
+     * @return
+     */
+    public static int getTextWidth(Font font, String text) {
+        return SwingUtilities2.getFontMetrics(null, font).stringWidth(text);
+    }
+
+    /**
+     * 获取字体像素高度
+     * @param font
+     * @return
+     */
+    public static int getFontHeight(Font font) {
+        return SwingUtilities2.getFontMetrics(null, font).getHeight();
+    }
+}
